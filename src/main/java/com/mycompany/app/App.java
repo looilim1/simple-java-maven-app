515da0d18d153911235fdf7a9956ecb89160e9d2
Looi Voon Lim <looi-voon.lim@keysight.com>
2023-01-16 16:51:47 +0800

    my fourth change to correct the error

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Sad World!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
