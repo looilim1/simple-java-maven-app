5a2ad814750bda3a0684f3ef276d2f6a6214d025
Looi Voon Lim <looi-voon.lim@keysight.com>
2023-01-16 16:50:43 +0800

    my third change in repo

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Sad World!";
 
     public App() {}
 
