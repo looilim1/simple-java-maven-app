a49ab8244f90c8bb344034d8d7e3037276f6beca
Looi Voon Lim <looi-voon.lim@keysight.com>
2023-01-16 16:43:33 +0800

    My second change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I Hate jenkins, so hard to use");
+        System.out.println("I Love jenkins, so easy to use");
     }
 
     public String getMessage() {
