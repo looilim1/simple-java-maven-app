df1f2a74143a25d8c4239bf99db822ab923b66e9
Looi Voon Lim <looi-voon.lim@keysight.com>
2023-01-17 09:32:49 +0800

    Hey I just did something here

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I Love jenkins, so easy to use");
+        System.out.println("I hope I will love Jenkins today");
     }
 
     public String getMessage() {
